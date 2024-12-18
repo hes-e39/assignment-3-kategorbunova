192862309da061e6c0617b24a34ea7f960528550
Kate Gorbunova <47249365+kategorbunova@users.noreply.github.com>
2024-12-18 11:29:01 -0800

    moving images to public folder w/o subfolders

--- src/components/workout/YourWorkoutList.tsx
+++ src/components/workout/YourWorkoutList.tsx
@@ -105,16 +105,16 @@ const YourWorkoutList: React.FC<WorkoutTimersListProps> = ({
                                         {(!isFinished || (!isStarted && timerElapsedTime === 0)) && (
                                             <Buttons style={{ marginLeft: '1rem' }}>
                                                 <SmallButton onClick={() => editTimer(index)}>
-                                                    <img src="/images/edit.png" alt="pencil edit icon" height="8px" />
+                                                    <img src="public/edit.png" alt="pencil edit icon" height="8px" />
                                                 </SmallButton>
                                                 {index !== 0 && (
                                                     <SmallButton onClick={() => moveTimerUp(index)}>
-                                                        <img src="/images/chevron.png" alt="chevron up" height="8px" />
+                                                        <img src="public/chevron.png" alt="chevron up" height="8px" />
                                                     </SmallButton>
                                                 )}
                                                 {index !== timersArray.length - 1 && (
                                                     <SmallButton onClick={() => moveTimerDown(index)}>
-                                                        <img src="/images/chevron.png" alt="chevron down" height="8px" />
+                                                        <img src="public/chevron.png" alt="chevron down" height="8px" />
                                                     </SmallButton>
                                                 )}
                                             </Buttons>
